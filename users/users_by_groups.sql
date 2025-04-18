b43beb8940aad6f8125f85c7ab20de178be0143c
Clay Oldham <97476273+rollplayr@users.noreply.github.com>
2025-04-18 16:11:40 -0500

    Update users_by_groups.sql

--- users/users_by_groups.sql
+++ users/users_by_groups.sql
@@ -1,9 +1,24 @@
-SELECT
-  g.desc as patron_group,
-  COUNT(CASE WHEN u.active = 'true' THEN 1 END) AS active_users,
-  COUNT(CASE WHEN u.active = 'false' THEN 1 END) AS non_active_users,
-  COUNT(*) AS total_users
-FROM folio_users.users__t u
-JOIN folio_users.groups__t g ON u.patron_group = g.id
-GROUP BY g.desc
-order BY g.desc ASC;
+--metadb:function users_by_groups
+
+CREATE FUNCTION users_by_groups(
+    start_date date DEFAULT '2000-01-01',
+    end_date date DEFAULT '2050-01-01'
+)
+RETURNS TABLE(
+    g.id text,
+    active_users integer,
+    non_active_users integer)
+AS $$
+ SELECT
+   g.desc as patron_group,
+   COUNT(CASE WHEN u.active = 'true' THEN 1 END) AS active_users,
+   COUNT(CASE WHEN u.active = 'false' THEN 1 END) AS non_active_users,
+   COUNT(*) AS total_users
+ FROM folio_users.users__t u
+ JOIN folio_users.groups__t g ON u.patron_group = g.id
+ GROUP BY g.desc
+ order BY g.desc ASC
+$$
+LANGUAGE SQL
+STABLE
+PARALLEL SAFE;
